4e9304ccbd5e8d5b3a763e2f96934a3d4f0ed9fe
leechinghua <leechenghua107@gmail.com>
2024-11-29 21:42:02 +0800

    toggleCompleted功能

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,15 @@
 import { MdDelete, MdEdit } from "react-icons/md";
 
-function Todo({ todo, deleteTodo }) {
+function Todo({ todo, deleteTodo, toggleCompleted }) {
   return (
-    <div className="todo">
-      <p>{todo.content}</p>
+    <div className={`todo ${todo.isCompleted ? "completed" : ""}`}>
+      <p
+        onClick={() => {
+          toggleCompleted(todo.id);
+        }}
+      >
+        {todo.content}
+      </p>
       <div>
         <MdEdit />
         <MdDelete
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -4,11 +4,11 @@ import Todo from "./Todo";
 
 function TodoWrapper() {
   const [todos, setTodos] = useState([
-    { content: "打掃廁所", id: Math.random() },
-    { content: "寫作業", id: Math.random() },
+    { content: "打掃廁所", id: Math.random(), isCompleted: false },
+    { content: "寫作業", id: Math.random(), isCompleted: false },
   ]);
   const addTodo = (content) => {
-    setTodos([...todos, { content, id: Math.random() }]);
+    setTodos([...todos, { content, id: Math.random(), isCompleted: false }]);
   };
   const deleteTodo = (id) => {
     setTodos(
@@ -17,12 +17,28 @@ function TodoWrapper() {
       })
     );
   };
+  const toggleCompleted = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id == id
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : todo;
+      })
+    );
+  };
   return (
     <div className="wrapper">
       <h1>待辦事項</h1>
       <CreateForm addTodo={addTodo} />
       {todos.map((todo) => {
-        return <Todo todo={todo} key={todo.id} deleteTodo={deleteTodo} />;
+        return (
+          <Todo
+            todo={todo}
+            key={todo.id}
+            deleteTodo={deleteTodo}
+            toggleCompleted={toggleCompleted}
+          />
+        );
       })}
     </div>
   );
