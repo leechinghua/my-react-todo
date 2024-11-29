056641a24bfcd536e2eb3710876909533d6bd24f
leechinghua <leechenghua107@gmail.com>
2024-11-29 16:12:43 +0800

    delete功能

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,18 @@
+import { MdDelete, MdEdit } from "react-icons/md";
 
-function Todo({ todo }) {
+function Todo({ todo, deleteTodo }) {
   return (
     <div className="todo">
       <p>{todo.content}</p>
+      <div>
+        <MdEdit />
+        <MdDelete
+          onClick={() => {
+            deleteTodo(todo.id);
+          }}
+          style={{ cursor: "pointer", marginLeft: "5px" }}
+        />
+      </div>
     </div>
   );
 }
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -10,12 +10,19 @@ function TodoWrapper() {
   const addTodo = (content) => {
     setTodos([...todos, { content, id: Math.random() }]);
   };
+  const deleteTodo = (id) => {
+    setTodos(
+      todos.filter((todo) => {
+        return todo.id !== id;
+      })
+    );
+  };
   return (
     <div className="wrapper">
       <h1>待辦事項</h1>
-      <CreateForm  addTodo={addTodo}/>
+      <CreateForm addTodo={addTodo} />
       {todos.map((todo) => {
-        return <Todo todo={todo} key={todo.id} />;
+        return <Todo todo={todo} key={todo.id} deleteTodo={deleteTodo} />;
       })}
     </div>
   );
